a86df7e65b956c509021abaee8d80f51a92e08d6
Austin Riendeau <austin@abstract.com>
2020-09-17 17:04:53 -0600

    better javascript cleanup

--- inits/02_javascript.el
+++ inits/02_javascript.el
@@ -2,25 +2,31 @@
   :straight t
   :mode (("\\.ts$" . typescript-mode))
   :mode (("\\.tsx$" . typescript-mode))
-  :init
-  (add-hook 'typescript-mode-hook 'prettier-js-mode))
+  :hook (typescript-mode . set-js-indentation))
 
 (use-package js2-mode
   :straight t
   :mode (("\\.js$" . js2-mode))
-  :init
-  (add-hook 'js2-mode-hook 'prettier-js-mode))
+  :hook (js2-mode . set-js-indentation))
 
 (use-package rjsx-mode
   :straight t
+  :hook (rjsx-mode . set-js-indentation)
+  :mode ("\\.jsx$" . rjsx-mode)
   :init
   (add-hook 'rsjx-mode-hook 'prettier-js-mode))
 
 (use-package prettier-js
+  :hook (rsjx-mode js2-mode typescript-mode)
   :straight t)
 
-(setq-default js2-indent-level 2
-	      rsjx-indent-level 2
-              css-indent-offset 2
-	      sass-indent-offset 2
-              typescript-indent-level 2)
+(defun set-js-indentation()
+  (setq indent-tabs-mode nil
+	;; indent-line-function 'insert-tab
+	tab-width 2
+	c-basic-offset 2
+	js2-indent-level 2
+	rsjx-indent-level 2
+        css-indent-offset 2
+        sass-indent-offset 2
+        typescript-indent-level 2))
